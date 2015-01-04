513374b89911f48ed3444bbd0d20d7482dede688
Eric Biesterfeld <eric@ebiester.com>
2015-01-03 20:37:25 -0500

    refactored tests

--- kata/rails/1/controller-oriented-design/spec/controllers/tickets_controller_spec.rb
+++ kata/rails/1/controller-oriented-design/spec/controllers/tickets_controller_spec.rb
@@ -1,82 +1,62 @@
 require 'rails_helper'
 
 describe TicketsController do
-  it "can add a new ticket" do
-    create(:villian)
-    create(:juniorsupport)
+  before(:all) do
+    @title = 'title'
+    @description = 'description'
+  end
 
-    villian = User.find_by(role: 'villian')
-    title = 'title'
-    description = 'description'
-
-    params = {:user => villian.username, 
-              :title => title,
-              :description => description }
+  def createTicket(submitting_user)
+    params = {:user => submitting_user.username, 
+              :title => @title,
+              :description => @description }
 
     post 'create', params
 
-    newticket = Ticket.find_by(title: title)
-    expect(newticket.title).to eq(title)
-    expect(newticket.description).to eq(description)
+    Ticket.find_by(title: @title)
+  end
+
+  it "can add a new ticket" do
+    villian = create(:villian)
+    create(:juniorsupport)
+
+    newticket = createTicket(villian)
+
+    expect(newticket.title).to eq(@title)
+    expect(newticket.description).to eq(@description)
     expect(newticket.submitter).to eq(villian)
   end
 
   it 'will assign an eligible ticket to an open employee' do
-    create(:villian)
+    villian = create(:villian)
     create(:juniorsupport)
+    newticket = createTicket(villian)
 
-    villian = User.find_by(role: 'villian')
-    title = 'title'
-    description = 'description'
-
-    params = {:user => villian.username, 
-              :title => title,
-              :description => description }
-
-    post 'create', params
-
-    newticket = Ticket.find_by(title: title)
-    expect(newticket.assigned).to be_an_instance_of(User)
-    expect(newticket.assigned.current_ticket).to eql(newticket)
+    expect(newticket.assigned).
+      to be_an_instance_of(User)
+    expect(newticket.assigned.current_ticket).
+      to eql(newticket)
   end
 
   it 'will not assign a ticket to a junior support member if the ticket is from a supervillian' do
-    create(:supervillian)
+    supervillian = create(:supervillian)
     create(:juniorsupport)
+    newticket = createTicket(supervillian)
 
-    supervillian = User.find_by(role: 'supervillian')
-    title = 'title'
-    description = 'description'
-
-    params = {:user => supervillian.username, 
-              :title => title,
-              :description => description }
-
-    post 'create', params
-
-    newticket = Ticket.find_by(title: title)
     expect(newticket.assigned).to be_nil
   end
 
-    it 'will assign a ticket to a senior support member if the ticket is from a supervillian' do
-    create(:supervillian)
+  it 'will assign a ticket to a senior support member if the ticket is from a supervillian' do
+    supervillian = create(:supervillian)
     create(:juniorsupport)
     create(:seniorsupport)
+    newticket = createTicket(supervillian)
 
-    supervillian = User.find_by(role: 'supervillian')
-    title = 'title'
-    description = 'description'
-
-    params = {:user => supervillian.username, 
-              :title => title,
-              :description => description }
-
-    post 'create', params
-
-    newticket = Ticket.find_by(title: title)
-    expect(newticket.assigned.role).to eql('seniorSupport')
-    expect(newticket.assigned.current_ticket).to eql(newticket)
+    expect(newticket.assigned.role).
+      to eql('seniorSupport')
+    expect(newticket.assigned.current_ticket).
+      to eql(newticket)
   end
 
-
+  it 'will update the state from open to in progress when updating the ticket'
 end
